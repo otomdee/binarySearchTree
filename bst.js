3d3843ec684496fc17e2a0e5ec3adb7600230cac
otomdee <otomdaniel65@gmail.com>
2024-09-18 09:28:35 +0100

    add insert function

---
+++ bst.js
@@ -0,0 +1,128 @@
+class Node {
+    constructor(data = null, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.array = array;
+        this.root = null;
+        this.insertRoot = null;
+    }
+
+    buildTree() {
+        //sort
+        let sortedArray = mergeSort(this.array);
+        //remove duplicates
+        for (let i = 0; i < sortedArray.length; i++) {
+            if (sortedArray[i] === sortedArray[i + 1]) {
+                sortedArray.splice(i, 1);
+            }
+        }
+        //arrange
+        this.root = arrange(sortedArray);
+    }
+
+    insert(data, root) {
+        let node = new Node(data);
+        //(this.insertRoot tracks the most recent parent Node)
+        //if the space is empty, place the item there
+        if (root === null) {
+            if (node.data < this.insertRoot.data) {
+                this.insertRoot.left = node;
+            }
+            else if (node.data > this.insertRoot.data) {
+                this.insertRoot.right = node;
+            }
+        }
+        //else, compare value and root
+        else {
+            if (data < root.data) {
+                this.insertRoot = root;
+                this.insert(data, root.left);
+            }
+            else if (data > root.data) {
+                this.insertRoot = root;
+                this.insert(data, root.right);
+            }
+        }
+    }
+}
+
+function arrange(array) {
+    //find middle element(root)
+    let index = Math.floor(array.length / 2);
+    root = array[index];
+    let node = new Node(root);
+
+    if (array.length > 1) {
+        node.left = arrange(array.slice(0, index));
+        node.right = arrange(array.slice(index + 1));
+        return node;
+    }
+    else {
+        return node;
+    }
+}
+
+function mergeSort(array) {
+    //if array length = 1, return
+    if (array.length == 1) return array;
+    //else
+    else {
+        let halfLength = Math.floor(array.length / 2);
+        let left = mergeSort(array.splice(0, halfLength))
+        let right = mergeSort(array);
+        
+        return merge(left, right);
+    }
+}
+
+function merge(left, right) {
+    const arraylength = left.length + right.length;
+    let finalArray = [];
+    while (finalArray.length < arraylength) {
+        if (left.length == 0) {
+            finalArray = finalArray.concat(right);
+        }
+        else if (right.length == 0) {
+            finalArray = finalArray.concat(left);
+        }
+        else {
+            if (left[0] < right[0]) {
+                finalArray.push(left[0]);
+                left.splice(0,1);
+            }
+            else {
+                finalArray.push(right[0]);
+                right.splice(0,1);
+            }
+        }
+    }
+    
+    return finalArray;
+}
+
+//pretty print function
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let test =  new Tree(testArray);
+test.buildTree();
+
+prettyPrint(test.root);
